6ee9c67422106015e78d3d687cd16955e1489901
xroah <396069583@qq.com>
2017-12-27 10:00:07 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const umd = require("gulp-umd");
+const css = require("gulp-clean-css");
+const concat = require("gulp-concat");
+
+gulp.task("minifyCss", () => {
+    return gulp.src("./src/rplayer.css")
+        .pipe(css())
+        .pipe(rename("rplayer.min.css"))
+        .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("concat", () => {
+    return gulp.src(["./src/dom.js", "./src/rplayer.js"])
+        .pipe(concat("rplayer.js"))
+        .pipe(umd({
+            exports: () => "RPlayer",
+            namespace: () => "RPlayer"
+        }))
+        .pipe(gulp.dest("./dist"))
+})
+
+gulp.task("uglifyJs", ["concat"], () => {
+    return gulp.src("./dist/rplayer.js")
+        .pipe(uglify({
+            output: {
+                comments: /^!/
+            }
+        }))
+        .pipe(rename("rplayer.min.js"))
+        .pipe(gulp.dest("./dist"));
+});
+
+gulp.watch("./src/*.*", ["concat", "uglifyJs", "minifyCss"]);
+
+gulp.task("default", ["concat", "uglifyJs", "minifyCss"]);
