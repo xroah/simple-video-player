0c3e19c48b84cb11b1f6d2b739277cec2e7eb6ab
X Roah <396069583@qq.com>
2017-12-29 23:56:23 +0800

    global variable

---
+++ src/global_var.js
@@ -0,0 +1,13 @@
+"use strict";
+var doc = document,
+    guid = 1,
+    SLIDER_SIZE = 12,
+    DEFAULT_OPTIONS = {
+    autoPlay: false,
+    defaultVolume: 50,
+    loop: false,
+    poster: "",
+    preload: "metadata",
+    source: "",
+    msg: ""
+};
