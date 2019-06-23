2db4c9e1b2e5bbba60cfa3918c0a086d8fb0da97
xroah <396069583@qq.com>
2019-06-23 19:55:46 +0800

    formatTime

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -9,3 +9,25 @@ export function isFunc(fn: any) {
 export function isPlainObject(obj: any) {
     return Object.prototype.toString.call(obj) === "[object Object]"
 }
+
+export function formatTime(seconds: number) {
+    const convert = (num: number) => (100 + num).toString().substr(1)
+    const hour = 60 * 60
+    seconds = Math.round(seconds)
+
+    if (seconds < 60) {
+        return `00:00:${convert(seconds)}`
+    } else if (seconds < hour) {
+        const min = convert(Math.floor(seconds / 60))
+        const sec = convert(seconds % 60)
+
+        return `00:${min}:${sec}`
+    }
+
+    const _hour = Math.floor(seconds / hour)
+    seconds = seconds % hour
+    const min = Math.floor(seconds / 60)
+    seconds = seconds % 60
+
+    return `${convert(_hour)}:${convert(min)}:${convert(seconds)}`
+}
