3cc3436c441fedec21ea01811f28389338831d13
xroah <396069583@qq.com>
2020-08-01 21:18:10 +0800

    volume

---
+++ src/ts/addons/popup.ts
@@ -0,0 +1,100 @@
+import {
+    addListener,
+    removeAllListeners,
+    removeListener
+} from "../dom-event";
+import RPlayer from "../player";
+import Transition from "../modules/transition";
+import {HIDDEN_CLASS} from "../constants";
+import {createEl} from "../utils";
+
+export default class Popup extends Transition {
+    el: HTMLElement
+    relatedTarget: HTMLElement | undefined
+    rp: RPlayer
+
+    constructor(rp: RPlayer, ...classes: string[]) {
+        super()
+
+        this.rp = rp
+        this.el = createEl("div", ...classes)
+        this.hideTimeout = 300
+    }
+
+    handleTransitionEnd = () => {
+        this.el.classList.add(HIDDEN_CLASS)
+        this.removeListeners()
+    }
+
+    addListeners() {
+        addListener(this.el, "mouseenter", this.handleMouseEnterLeave)
+        addListener(this.el, "mouseleave", this.handleMouseEnterLeave)
+
+        if (this.relatedTarget) {
+            addListener(this.relatedTarget, "mouseleave", this.handleMouseEnterLeave)
+        }
+    }
+
+    removeListeners() {
+        removeListener(this.el, "mouseenter", this.handleMouseEnterLeave)
+        removeListener(this.el, "mouseleave", this.handleMouseEnterLeave)
+        removeListener(this.el, "transitionend", this.handleTransitionEnd)
+
+        if (this.relatedTarget) {
+            removeListener(this.relatedTarget, "mouseleave", this.handleMouseEnterLeave)
+        }
+    }
+
+    handleIconClick = () => {
+        this.setVisible(!this.visible)
+    }
+
+    handleControlBarHidden = () => {
+        this.setVisible(false, true)
+    }
+
+    handleMouseEnterLeave = (evt: MouseEvent) => {
+        const isEnter = evt.type === "mouseenter"
+
+        if (isEnter) {
+            this.clearTimeout()
+        } else {
+            if (evt.target === this.el) {
+                this.setVisible(false)
+            } else {
+                //prevent from hiding immediately once mouse leave relatedTarget
+                this.delayHide()
+            }
+        }
+    }
+
+    setVisible(visible: boolean, force = false) {
+        if (this.visible === visible) {
+            return
+        }
+
+        super.setVisible(visible)
+        this.removeListeners()
+
+        if (visible) {
+            this.addListeners()
+
+            return
+        }
+
+        if (force) {
+            this.handleTransitionEnd()
+        } else {
+            addListener(this.el, "transitionend", this.handleTransitionEnd)
+        }
+    }
+
+    destroy() {
+        this.off()
+        removeAllListeners(this.el)
+
+        if (this.relatedTarget) {
+            removeAllListeners(this.relatedTarget)
+        }
+    }
+}
---
+++ src/ts/addons/volume.ts
@@ -0,0 +1,107 @@
+import RPlayer from "..";
+import {addListener} from "../dom-event";
+import Slider from "../modules/slider";
+import {HIDDEN_CLASS} from "../constants";
+import Popup from "./popup";
+import {createEl} from "../utils";
+
+class Volume extends Popup {
+    private _slider: Slider
+    private _text: HTMLElement
+    private _wrapper: HTMLElement
+
+    constructor(rp: RPlayer) {
+        super(rp, "rplayer-volume-popup", HIDDEN_CLASS)
+
+        this._wrapper = createEl("div", "rplayer-volume-slider")
+        this._slider = new Slider(this._wrapper, {
+            vertical: true
+        })
+        this._text = document.createElement("span")
+        //the icon btn, for showing the volume popup
+        this.relatedTarget = createEl("span", "rplayer-volume-btn", "rplayer-addon-btn")
+        this.rp = rp
+    }
+
+    updateVolume = () => {
+        const {
+            rp: {video},
+            relatedTarget: btn
+        } = this
+        const val = video.isMuted() ? 0 : video.getVolume()
+
+        if (val === 0) {
+            btn!.classList.add("rplayer-muted")
+        } else {
+            btn!.classList.remove("rplayer-muted")
+        }
+
+        this.update(val * 100)
+    }
+
+    handleIconClick = () => {
+        const {video} = this.rp
+
+        if (this.visible) {
+            video.setMuted(!video.isMuted())
+        } else {
+            this.setVisible(!this.visible)
+        }
+
+        this.updateVolume()
+    }
+
+    handleSliderValueChange = (evt: any) => {
+        const val = evt.value / 100
+
+        this.rp.video.setVolume(val)
+    }
+
+    initEvents() {
+        const {
+            rp,
+            relatedTarget: btn
+        } = this
+
+        addListener(btn!, "click", this.handleIconClick)
+        rp.control.bar.on("hidden", this.handleControlBarHidden)
+        rp.on("volumechange", this.updateVolume)
+        this._slider.on("valuechange", this.handleSliderValueChange)
+    }
+
+    mountTo(container: HTMLElement) {
+        const btn = this.relatedTarget!
+
+        this.el.appendChild(this._text)
+        this.el.appendChild(this._wrapper)
+        container.appendChild(btn)
+        container.appendChild(this.el)
+
+        this.initEvents()
+    }
+
+    //update slider and text, when volumechange or mute
+    update(val: number) {
+        //prevent racing(volume change and slider value change(moving) simultaneously)
+        if (!this._slider.isMoving()) {
+            this._slider.update(val)
+        }
+
+        this._text.innerHTML = Math.round(val).toString()
+    }
+}
+
+export default (rp: RPlayer) => {
+    const el = createEl("div", "rplayer-volume-wrapper")
+    const volume = new Volume(rp)
+
+    volume.mountTo(el)
+
+    rp
+        .on("beforemount", () => {
+            const {right} = rp.getAddonContainers()
+
+            right.appendChild(el)
+        })
+        .on("destroy", () => volume.destroy())
+}
