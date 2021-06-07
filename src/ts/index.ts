93384f5369c24db4fc3ad1db54dcafd87c03efb3
xroah <396069583@qq.com>
2021-06-07 14:41:24 +0800

    add addons and plugins to player

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,47 @@
-export {default} from "./player"
+import Player from "./player"
+import fullscreenBtn from "./addons/fullscreen-btn"
+import pictureInPicture from "./addons/picture-in-picture"
+import playbackRate from "./addons/playback-rate"
+import playerSettings from "./addons/player-settings"
+import volume from "./addons/volume"
+import miniProgress from "./plugins/mini-progress"
+import hotkey from "./plugins/hotkey"
+
+interface Addons {
+    fullscreenBtn: typeof fullscreenBtn
+    pictureInPicture: typeof pictureInPicture
+    playbackRate: typeof playbackRate
+    playerSettings: typeof playerSettings
+    volume: typeof volume
+}
+
+interface Plugins {
+    miniProgress: typeof miniProgress
+    hotkey: typeof hotkey
+}
+
+interface Props {
+    addons: Addons
+    plugins: Plugins
+}
+
+type PlayerType = typeof Player & Props
+
+const RPlayer = Player as PlayerType
+
+RPlayer.addons = {
+    fullscreenBtn,
+    pictureInPicture,
+    playbackRate,
+    playerSettings,
+    volume
+}
+
+RPlayer.plugins = {
+    miniProgress,
+    hotkey
+}
+
+export {Player}
+
+export default RPlayer
