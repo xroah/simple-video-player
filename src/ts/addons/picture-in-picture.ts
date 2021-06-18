a48a126a52a99814af5136477481543a0fc28134
xroah <396069583@qq.com>
2021-06-19 00:39:16 +0800

    toggle picture in picture button icon

--- src/ts/addons/picture-in-picture.ts
+++ src/ts/addons/picture-in-picture.ts
@@ -1,13 +1,32 @@
 import { Player } from "..";
+import { addListeners } from "../commons/dom-event";
 
 export default {
     classNames: ["rplayer-pip-btn"],
-    init() {
+    init(this: HTMLElement, p: Player) {
         if (!("pictureInPictureEnabled" in document)) {
             console.warn("The Picture-in-Picture Web API is not available.")
 
             return false
         }
+
+        const handlePipLeaveOrEnter = (evt: Event) => {
+            const CLASS = "rplayer-pip-entered"
+
+            if (evt.type === "enterpictureinpicture") {
+                this.classList.add(CLASS)
+            } else {
+                this.classList.remove(CLASS)
+            }
+        }
+
+        addListeners(
+            p.video.el,
+            {
+                enterpictureinpicture: handlePipLeaveOrEnter,
+                leavepictureinpicture: handlePipLeaveOrEnter
+            }
+        )
     },
     action(p: Player) {
         const { video: { el } } = p
