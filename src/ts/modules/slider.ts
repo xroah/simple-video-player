38c191efbe52c91026a24752f628498d935d86fa
xroah <396069583@qq.com>
2021-06-21 21:03:18 +0800

    remove tooltip and secondary element

--- src/ts/modules/slider.ts
+++ src/ts/modules/slider.ts
@@ -2,36 +2,26 @@ import EventEmitter from "../commons/event-emitter"
 import { isPlainObject, createEl } from "../commons/utils"
 import {
     addListener,
-    addListeners,
     removeAllListeners,
     removeListener
 } from "../commons/dom-event"
 import { MOVING_CLASS } from "../commons/constants"
-import Tooltip from "./tooltip"
 
 interface Options {
     vertical?: boolean
     defaultValue?: number | string
-    secondary?: boolean
-    tooltip?: {
-        formatter: (v: number) => string
-    }
 }
 
 export default class Slider extends EventEmitter {
     private _vertical: boolean
     private _el: HTMLElement
     private _marker: HTMLElement
-    private _secondaryProgress: HTMLElement | null = null
     private _primaryProgress: HTMLElement
-    private _tooltip: Tooltip | undefined
-    private _tooltipFormatter: ((v: number) => string) | undefined
     private _value = 0
 
     private _startX = 0
     private _startY = 0
     private _moving = false
-    private _mouseEntered = false
 
     constructor(container: HTMLElement, options?: Options) {
         super()
@@ -48,16 +38,7 @@ export default class Slider extends EventEmitter {
             this._vertical ? "rplayer-slider-wrapper-vertical" : ""
         )
         this._marker = createEl("div", "rplayer-slider-marker")
-        this._primaryProgress = createEl("div", "rplayer-slider-primary-progress")
-
-        if (options.secondary) {
-            this._secondaryProgress = createEl("div", "rplayer-slider-secondary-progress")
-        }
-
-        if (options.tooltip) {
-            this._tooltip = new Tooltip(this._el)
-            this._tooltipFormatter = options.tooltip.formatter
-        }
+        this._primaryProgress = createEl("div", "rplayer-slider-primary")
 
         this.update(this._value)
         this.initEvents()
@@ -66,10 +47,6 @@ export default class Slider extends EventEmitter {
 
     private mountTo(container: HTMLElement) {
         const track = createEl("div", "rplayer-slider-track")
-
-        if (this._secondaryProgress) {
-            track.appendChild(this._secondaryProgress)
-        }
 
         track.appendChild(this._primaryProgress)
         track.appendChild(this._marker)
@@ -80,17 +57,6 @@ export default class Slider extends EventEmitter {
     private initEvents() {
         addListener(this._el, "mousedown", this.handleMouseDown)
         // addListener(this._el, "touchstart", this.handleMouseDown)
-
-        if (this._tooltip) {
-            addListeners(
-                this._el,
-                {
-                    mouseenter: this.handleMouseEnter,
-                    mousemove: this.handleMouseMove,
-                    mouseleave: this.handleMouseLeave
-                }
-            )
-        }
     }
 
     getValue() {
@@ -128,79 +94,11 @@ export default class Slider extends EventEmitter {
         }
     }
 
-    updateSecondary(val: number) {
-        const percent = `${val}%`
-
-        if (this._secondaryProgress) {
-            const prop = this._vertical ? "height" : "width"
-            this._secondaryProgress.style[prop] = percent
-        }
-    }
-
     private getPercent(val: number) {
         const elRect = this._el.getBoundingClientRect()
         const v = this._vertical ? val / elRect.height : val / elRect.width
 
         return v * 100
-    }
-
-    private updateTooltip(pos = 0, isPercent = false) {
-        const tp = this._tooltip
-
-        if (!tp) {
-            return
-        }
-
-        let percent = 0
-
-        if (isPercent) {
-            percent = pos
-        } else {
-            if (this._vertical) {
-                percent = pos / this._el.offsetHeight * 100
-            } else {
-                percent = pos / this._el.offsetWidth * 100
-            }
-        }
-
-        tp.updateText(this._tooltipFormatter!(percent))
-        tp.updatePosition(pos)
-    }
-
-    private handleMouseMove = (evt: MouseEvent) => {
-        this.handleMouseEnter(evt)
-    }
-
-    private handleMouseEnter = (evt: MouseEvent) => {
-        if (this._moving) {
-            return
-        }
-
-        const x = evt.clientX
-        const y = evt.clientY
-        const elRect = this._el.getBoundingClientRect()
-        let pos = 0
-        this._mouseEntered = true
-
-        if (this._vertical) {
-            pos = elRect.bottom - y
-        } else {
-            pos = x - elRect.left
-        }
-
-        if (this._tooltip) {
-            this._tooltip.setVisible(true)
-        }
-
-        this.updateTooltip(pos)
-    }
-
-    private handleMouseLeave = (evt: MouseEvent) => {
-        if (!this._moving && this._tooltip) {
-            this._tooltip.setVisible(false)
-        }
-
-        this._mouseEntered = false
     }
 
     private handleMouseDown = (evt: any) => {
@@ -224,7 +122,6 @@ export default class Slider extends EventEmitter {
             }
 
             this.emit("slidestart", percentVal)
-            this.updateTooltip(val)
             addListener(document, "mousemove", this.handleSliderMove)
             // addListener(document, "touchmove", this.handleSliderMove)
             addListener(document, "mouseup", this.handleMouseUp, { once: true })
@@ -297,16 +194,11 @@ export default class Slider extends EventEmitter {
         
         this.emit("slidemove", percentVal)
         this.updateAndEmit(percentVal)
-        this.updateTooltip(percentVal, true)
     }
 
     private handleMouseUp = () => {
         removeListener(document, "mousemove", this.handleSliderMove)
         // removeListener(document, "touchmove", this.handleSliderMove)
-
-        if (!this._mouseEntered && this._tooltip) {
-            this._tooltip.setVisible(false)
-        }
 
         if (this._moving) {
             this._moving = false
