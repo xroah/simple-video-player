d4d4547a8f9e468d8a53b7870a9a26a77f3d6400
xroah <396069583@qq.com>
2021-07-12 20:37:12 +0800

    amend settings

--- src/ts/addons/player-settings.ts
+++ src/ts/addons/player-settings.ts
@@ -19,16 +19,147 @@ const REMEMBER_ID_PREFIX = "rplayer-remember-last-position"
 const SETTINGS_KEY = "__RPLAYER_SETTINGS__"
 const TIME_KEY = "__RPLAYER_CURRENT_TIME__"
 
+interface Options {
+    showUI?: boolean
+    autoplay?: boolean
+    rememberPosition?: boolean
+    loop?: boolean
+}
+
+function getLocalData(key: string) {
+    const settings = localStorage.getItem(key)
+    const empty = {}
+
+    if (settings) {
+        try {
+            const json = JSON.parse(settings)
+
+            return json
+        } catch (error) {
+            return empty
+        }
+    }
+
+    return empty
+}
+
+class SimpleSettings {
+    constructor(private _player: Player, private _options: Options) {
+        this.handleSettings(_options)
+
+        _player.on("loadedmetadata", this.handleLoad)
+    }
+
+    private handleLoad = () => {
+        const {
+            autoplay,
+            rememberPosition
+        } = this._options
+        const {
+            _player,
+            _player: { video }
+        } = this
+
+        if (autoplay && video.paused) {
+            video
+                .play()
+                .catch(
+                    () => _player.message.show("浏览器已阻止自动播放")
+                )
+        }
+
+        if (rememberPosition) {
+            const saved = getLocalData(TIME_KEY)
+            const key = this.getVideoKey()
+            const time = saved[key]
+
+            if (time) {
+                video.currentTime = time
+                _player.message.show(`已为您跳转至上次播放位置: ${formatTime(time)}`)
+            }
+        }
+    }
+
+    private addTimeUpdateEvent() {
+        this._player.on("timeupdate", this.handleTimeUpdate)
+    }
+
+    private removeTimeUpdateEvent() {
+        this._player.off("timeupdate", this.handleTimeUpdate)
+    }
+
+    private getVideoKey() {
+        const url = this._player.video.currentSrc
+        const a = document.createElement("a")
+        let key = ''
+
+        //parse the url
+        a.href = url
+
+        if (a.host) {
+            key = `${a.protocol}//${a.host}${a.pathname}`
+        } else {
+            //maybe blob
+            key = url
+        }
+
+        return window.btoa(key)
+    }
+
+    private saveTimeToLocal = () => {
+        const saved = <any>getLocalData(TIME_KEY)
+        const key = this.getVideoKey()
+
+        saved[key] = this._player.video.currentTime
+
+        localStorage.setItem(TIME_KEY, JSON.stringify(saved))
+    }
+
+    //save current time to localStorage
+    private handleTimeUpdate = throttle(
+        this.saveTimeToLocal,
+        {
+            trailing: false,
+            delay: 1000
+        }
+    )
+
+    handleSettings(settings: object) {
+        const {
+            autoplay,
+            loop,
+            rememberPosition
+        } = <any>settings
+        const { _player: { video } } = this
+
+        video.autoplay = autoplay
+        video.loop = loop
+
+        if (rememberPosition) {
+            this.addTimeUpdateEvent()
+        } else {
+            this.removeTimeUpdateEvent()
+
+            localStorage.removeItem(TIME_KEY)
+        }
+
+        this._options = settings
+    }
+}
+
 class PlayerSettings extends Popup {
     private _uid = uid++
     private _switches: Map<string, Switch> = new Map()
+    private _simpleSettings: SimpleSettings
 
-    constructor(p: Player) {
+    constructor(p: Player, private _options: Options) {
         super(
             p,
             classNames.addons.SETTINGS_POPUP,
             HIDDEN_CLASS
         )
+
+        this._simpleSettings = new SimpleSettings(p, _options)
 
         this.init()
     }
@@ -80,143 +211,39 @@ class PlayerSettings extends Popup {
     }
 
     private init() {
+        const localSettings = getLocalData(SETTINGS_KEY)
+        const settings = {
+            ...this._options,
+            ...localSettings
+        }
+
+        delete settings.showUI
+
         this.mount()
 
-        this.handleSettings()
+        this._simpleSettings.handleSettings(settings)
         this.initSwitchStatus()
-
-        this.player.on("loadedmetadata", this.handleLoad)
     }
 
-    private handleLoad = () => {
-        const {
-            autoplay,
-            rememberPosition
-        } = <any>this.getLocalData(SETTINGS_KEY)
-        const {
-            player,
-            player: { video }
-        } = this
-
-        if (autoplay) {
-            video
-                .play()
-                .catch(
-                    () => player.message.show("浏览器已阻止自动播放")
-                )
-        }
-
-        if (rememberPosition) {
-            const saved = this.getLocalData(TIME_KEY)
-            const key = this.getVideoKey()
-            const time = saved[key]
-
-            if (time) {
-                video.currentTime = time
-                player.message.show(`已为您跳转至上次播放位置: ${formatTime(time)}`)
-            }
-        }
-    }
-
-    private getLocalData(key: string) {
-        const settings = localStorage.getItem(key)
-        const empty = {}
-
-        if (settings) {
-            try {
-                const json = JSON.parse(settings)
-
-                return json
-            } catch (error) {
-                return empty
-            }
-        }
-
-        return empty
-    }
 
     private getId(prefix: string) {
         return `${prefix}-${this._uid}`
     }
 
     private initSwitchStatus() {
-        const localSettings = <any>this.getLocalData(SETTINGS_KEY)
+        const localSettings = <any>getLocalData(SETTINGS_KEY)
         const check = (s?: Switch) => s && s.check(true)
         const keyMap = new Map([
-            ["autoplay", this.getId(AUTOPLAY_ID_PREFIX)], 
-            ["rememberPosition", this.getId(REMEMBER_ID_PREFIX)], 
+            ["autoplay", this.getId(AUTOPLAY_ID_PREFIX)],
+            ["rememberPosition", this.getId(REMEMBER_ID_PREFIX)],
             ["loop", this.getId(LOOP_ID_PREFIX)]
         ])
-        
+
         keyMap.forEach((v, k) => {
             if (localSettings[k]) {
                 check(this._switches.get(v))
             }
         })
-    }
-
-    private addTimeUpdateEvent() {
-        this.player.on("timeupdate", this.handleTimeUpdate)
-    }
-
-    private removeTimeUpdateEvent() {
-        this.player.off("timeupdate", this.handleTimeUpdate)
-    }
-
-    private getVideoKey() {
-        const url = this.player.video.currentSrc
-        const a = document.createElement("a")
-        let key = ''
-
-        //parse the url
-        a.href = url
-
-        if (a.host) {
-            key = `${a.protocol}//${a.host}${a.pathname}`
-        } else {
-            //maybe blob
-            key = url
-        }
-
-        return window.btoa(key)
-    }
-
-    private saveTimeToLocal = () => {
-        const saved = <any>this.getLocalData(TIME_KEY)
-        const key = this.getVideoKey()
-
-        saved[key] = this.player.video.currentTime
-
-        localStorage.setItem(TIME_KEY, JSON.stringify(saved))
-    }
-
-    //save current time to localStorage
-    private handleTimeUpdate = throttle(
-        this.saveTimeToLocal,
-        {
-            trailing: false,
-            delay: 1000
-        }
-    )
-
-    private handleSettings(settings?: object) {
-        const {
-            autoplay,
-            loop,
-            rememberPosition
-        } = <any>(settings || this.getLocalData(SETTINGS_KEY))
-        const { player: { video } } = this
-
-        video.autoplay = autoplay
-        video.loop = loop
-
-        if (rememberPosition) {
-            this.addTimeUpdateEvent()
-        } else {
-            this.removeTimeUpdateEvent()
-
-            localStorage.removeItem(TIME_KEY)
-        }
     }
 
     private handleSwitchChange = (evt: EventObject) => {
@@ -230,16 +257,16 @@ class PlayerSettings extends Popup {
             [this.getId(LOOP_ID_PREFIX), "loop"]
         ])
         const key = idMap.get(id)
-        let localSettings = <any>this.getLocalData(SETTINGS_KEY)
+        let localSettings = <any>getLocalData(SETTINGS_KEY)
 
-        if(key) {
+        if (key) {
             localSettings[key] = checked
         }
 
         //save settings to localStorage
         localStorage.setItem(SETTINGS_KEY, JSON.stringify(localSettings))
 
-        this.handleSettings(localSettings)
+        this._simpleSettings.handleSettings(localSettings)
     }
 
     destroy() {
@@ -249,8 +276,17 @@ class PlayerSettings extends Popup {
 
 export default {
     classNames: [classNames.addons.SETTINGS_BTN],
-    init(this: HTMLElement, p: Player) {
-        const addon = new PlayerSettings(p)
+    init(this: HTMLElement, p: Player, options: Options = {}) {
+        let addon: SimpleSettings | PlayerSettings
+
+        if (!options.showUI) {
+            addon = new SimpleSettings(p, options)
+
+            return
+        }
+
+        addon = new PlayerSettings(p, options)
+
 
         addListeners(this, {
             mouseleave: handleMouseLeave.bind(addon),
