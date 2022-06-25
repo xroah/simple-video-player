c2f91dd0b5b2265273fa3630a533d35c3c1ef467
xroah <396069583@qq.com>
2022-06-25 14:53:11 +0800

    hotkey

---
+++ src/extentions/hotkey.ts
@@ -0,0 +1,84 @@
+import Video from "../modules/video"
+import { toggleFullScreen } from "../utils"
+
+export default class Hotkey {
+    constructor(
+        private _target: HTMLElement,
+        private _video: Video
+    ) {
+        _target.addEventListener("keydown", this.handleKeydown)
+    }
+
+    private handleKeydown = (evt: KeyboardEvent) => {
+        const key = evt.key.toLowerCase()
+        const v = this._video
+
+        switch (key) {
+            case "arrowdown":
+                this.setVolume(false)
+                break
+            case "arrowup":
+                this.setVolume()
+                break
+            case "arrowleft":
+                this.fastSeek(false)
+                break
+            case "arrowright":
+                this.fastSeek()
+                break
+            case " ": //space key
+                v.toggle()
+                break
+            case "m":
+                const muted = !v.isMuted()
+                v.setMuted(muted)
+                break
+            case "enter": // fullscreen
+                toggleFullScreen(this._target)
+        }
+    }
+
+    setVolume(add = true) {
+        const STEP = 5
+        const v = this._video
+        let volume = v.getVolume()
+
+        if (add) {
+            volume += STEP
+        } else {
+            volume -= STEP
+        }
+
+        if (volume < 0) {
+            volume = 0
+        } else if (volume > 100) {
+            volume = 100
+        }
+
+        v.setMuted(false)
+        v.setVolume(volume)
+    }
+
+    fastSeek(forward = true) {
+        const v = this._video
+        const STEP = 5
+        const duration = v.getDuration()
+        let curTime = v.getCurrentTime()
+
+        if (forward) {
+            curTime += STEP
+        } else {
+            curTime -= STEP
+        }
+
+        if (curTime < 0) {
+            curTime = 0
+        } else if (curTime > duration) {
+            curTime = duration
+        }
+
+        v.setCurrentTime(curTime)
+        //update the progress, if the keys were press for long time
+        //the timeupdate may not fire (waiting)
+    }
+}
