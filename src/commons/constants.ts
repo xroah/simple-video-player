5857db3179b9b10cd44534cfb8c08580f273e1d4
xroah <396069583@qq.com>
2022-10-23 14:29:18 +0800

    time info

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -4,6 +4,9 @@ export const SHOW_CLASS = "rplayer-show"
 
 export const controlBarHtml = `
     <div class="rplayer-progress-wrapper">
+        <div class="rplayer-current-time"></div>
+        <div class="rplayer-progress"></div>
+        <div class="rplayer-duration"></div>
     </div>
     <div class="rplayer-controls-wrapper">
         <div class="rplayer-left-controls"></div>
--- src/modules/control-bar.ts
+++ src/modules/control-bar.ts
@@ -13,6 +13,8 @@ interface Options {
 export default class ControlBar extends Transition {
     private _slider: Slider
     private _miniProgress?: MiniProgress
+    private _currentTimeEl: HTMLElement
+    private _durationEl: HTMLElement
 
     constructor(
         private _parent: HTMLElement,
@@ -21,17 +23,26 @@ export default class ControlBar extends Transition {
     ) {
         super("rplayer-control-bar")
 
+        const DEFAULT_TIME = "00:00"
         this.el.innerHTML = controlBarHtml
         this.autoHide = true
         this.hideTimeout = 3000
         const sliderWrapper = this.el.querySelector(
-            ".rplayer-progress-wrapper"
+            ".rplayer-progress"
         ) as HTMLElement
+        this._currentTimeEl = this.el.querySelector(
+            ".rplayer-current-time"
+        ) as HTMLElement
+        this._durationEl = this.el.querySelector(
+            ".rplayer-duration"
+        ) as HTMLElement
+        this._currentTimeEl.innerHTML = DEFAULT_TIME
+        this._durationEl.innerHTML = DEFAULT_TIME
         this._slider = new Slider(
             sliderWrapper,
-            { 
+            {
                 buffer: true,
-                tooltip: this._formatTooltip 
+                tooltip: this._formatTooltip
             }
         )
 
@@ -51,6 +62,10 @@ export default class ControlBar extends Transition {
     protected init() {
         super.init()
         this._video.addListener("timeupdate", this._handleTimeUpdate)
+        this._video.addListener(
+            "durationchange",
+            this._handleDurationChange
+        )
         this._slider.on("value-change", this._handleSliderChange)
 
         if (this._miniProgress) {
@@ -80,12 +95,25 @@ export default class ControlBar extends Transition {
 
     private _hideMiniProgress = () => this._miniProgress?.hide()
 
+    private _handleDurationChange = () => {
+        const duration = this._video.getDuration()
+
+        if (duration) {
+            this._durationEl.innerHTML = formatTime(
+                Math.floor(duration)
+            )
+        }
+    }
+
     private _handleTimeUpdate = () => {
         if (this._slider.isMoving()) {
             return
         }
 
         const progress = this._video.getProgress()
+        this._currentTimeEl.innerHTML = formatTime(
+            Math.floor(this._video.getCurrentTime())
+        )
 
         this._slider.updateProgress(progress)
         this._miniProgress?.update(progress)
