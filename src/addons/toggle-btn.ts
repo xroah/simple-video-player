8f710022d48abbe9bff81d67ba86b59ea91fc0f7
xroah <396069583@qq.com>
2022-11-19 15:05:55 +0800

    toggle-btn addon

---
+++ src/addons/toggle-btn.ts
@@ -0,0 +1,35 @@
+import { Addon } from "../commons/types";
+import Player from "../modules/player";
+
+const toggleBtn: Addon = {
+    tag: "button",
+    classNames: ["rplayer-addon-btn", "rplayer-toggle-btn"],
+    install(
+        el: HTMLElement,
+        player: Player
+    ) {
+        const PAUSED_CLASS = "rplayer-paused"
+
+        if (player.video.isPaused()) {
+            el.classList.add(PAUSED_CLASS)
+        }
+
+        player.video
+            .addListener(
+                "play",
+                () => {
+                    el.classList.remove(PAUSED_CLASS)
+                }
+            )
+            .addListener(
+                "pause",
+                () => {
+                    el.classList.add(PAUSED_CLASS)
+                }
+            )
+
+        el.addEventListener("click", player.togglePlay)
+    }
+}
+
+export default toggleBtn
