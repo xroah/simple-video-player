fd99a0c5400791636b4c2ad0f839b7f1752d8de7
xroah <396069583@qq.com>
2022-11-29 21:16:04 +0800

    remove  controlBarHtml constant

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -1,11 +1,3 @@
 export const HIDDEN_CLASS = "rplayer-hidden"
 
 export const SHOW_CLASS = "rplayer-show"
-
-export const controlBarHtml = `
-    <div class="rplayer-progress-wrapper">
-        <div class="rplayer-current-time"></div>
-        <div class="rplayer-progress"></div>
-        <div class="rplayer-duration"></div>
-    </div>
-`
