deb77dd5ea7f0a5916be1d346fcd32644b6ccfcf
xroah <396069583@qq.com>
2023-04-17 21:02:15 +0800

    refactor: rename

--- src/addons/toggle.ts
+++ src/addons/toggle.ts
@@ -1,17 +1,18 @@
 import { ADDON_BTN_CLASS } from "../commons/constants"
 import { Addon } from "../commons/types"
 
-const togglePlay: Addon = {
+const toggle: Addon = {
     tag: "button",
     classNames: [ADDON_BTN_CLASS, "rplayer-toggle-btn"],
     install(el, player) {
         const PAUSED_CLASS = "rplayer-paused"
+        const {video} = player
 
-        if (player.video.isPaused()) {
+        if (video.isPaused()) {
             el.classList.add(PAUSED_CLASS)
         }
 
-        player.video
+        video
             .addListener(
                 "play",
                 () => el.classList.remove(PAUSED_CLASS)
@@ -21,8 +22,8 @@ const togglePlay: Addon = {
                 () => el.classList.add(PAUSED_CLASS)
             )
 
-        el.addEventListener("click", player.togglePlay.bind(player))
+        el.addEventListener("click", () => video.toggle())
     }
 }
 
-export default togglePlay
+export default toggle
