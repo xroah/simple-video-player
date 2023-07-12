fd95253b8a72a84a5ce74343fe5d0643064a76f8
xroah <396069583@qq.com>
2023-07-12 20:26:35 +0800

    feat: tray and window close event

--- desktop-client/src-tauri/src/main.rs
+++ desktop-client/src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use rplayer::tray;
+use tauri::WindowEvent;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -12,6 +13,14 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .system_tray(tray::build())
+        .on_system_tray_event(tray::handle_system_tray_event)
+        .on_window_event(|event| match event.event() {
+            WindowEvent::CloseRequested { api, .. } => {
+                let _ = event.window().hide();
+                api.prevent_close();
+            }
+            _ => (),
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
