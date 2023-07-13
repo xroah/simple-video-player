800bb45ea44714ea926a21767e27ac4eb36b88cf
xroah <396069583@qq.com>
2023-07-13 21:38:52 +0800

    chore: emotion

--- desktop-client/src/app.tsx
+++ desktop-client/src/app.tsx
@@ -1,21 +1,6 @@
-import { useEffect } from "react"
-import {TauriEvent, listen} from "@tauri-apps/api/event"
-
 function App() {
-    useEffect(
-        () => {
-            listen(TauriEvent.WINDOW_CLOSE_REQUESTED, e => {
-                if (e.windowLabel === "main") {
-                    console.log("closing")
-                }
-            })
-        },
-        []
-    )
-
     return (
-        <div className="container">
-            
+        <div>
         </div>
     )
 }
